b304cb2399e2e913d75eb0cc09c0c8f86ce48de8
U039b <git@0x39b.fr>
2025-07-31 18:16:21 +0200

    Update theme

--- export_theme_icons.py
+++ export_theme_icons.py
@@ -1,17 +1,18 @@
 import json
 
 
+# Source: https://icon-sets.iconify.design/tabler/
 def export_theme_icons():
-    stroke = 5
-    size = 24
+    stroke = 2
+    size = 36
     with open("colander_data_converter/data/themes/default.json", "r") as json_file:
         theme = json.load(json_file)
     for name, elt in theme["types"].items():
         with open(f"docs/_static/icons/{name}.svg", "w") as svg_file:
             svg = elt["svg_icon"]
             color = elt["fg_color"]
-            svg = svg.replace('stroke-width="4"', f'stroke-width="{stroke}"')
-            svg = svg.replace('width="48" height="48"', f'width="{size}" height="{size}"')
+            svg = svg.replace('stroke-width="2"', f'stroke-width="{stroke}"')
+            svg = svg.replace('width="24" height="24"', f'width="{size}" height="{size}"')
             svg = svg.replace("currentColor", color)
             svg_file.write(svg)
 
